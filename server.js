ef8ee6b159710a008264c47981ba7f08e8db1c63
Alex Souza <149825927+bl4ckzin190@users.noreply.github.com>
2025-12-12 09:52:28 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 const express = require("express");
-const cors = require("cors");
-
+const path = require("path");
 const app = express();
-app.use(cors());
-app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.send("API funcionando!");
-});
 
 const PORT = process.env.PORT || 3000;
+
+// Servir arquivos estáticos
+app.use(express.static(path.join(__dirname)));
+
+// Rota principal
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+// Iniciar servidor
 app.listen(PORT, () => {
   console.log("Servidor rodando na porta " + PORT);
 });
